55af9729d00ffe7c23f1577c4dd9136b85a3fda4
theB3NNDS <78061518+theB3NNDS@users.noreply.github.com>
2024-05-03 17:44:46 +0800

    feat: implemented basic player movement

---
+++ Burger Jumper/Assets/playerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    public InputActionReference move;
+    public InputActionReference jump;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(x:moveDirection.x * moveSpeed, y:rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void OnEnable()
+    {
+        jump.action.performed += PerformJump;
+    }
+
+    private void OnDisable()
+    {
+        jump.action.performed -= PerformJump;
+    }
+
+    private void PerformJump (InputAction.CallbackContext obj)
+    {
+        Debug.Log("Jumped");
+        Jump();
+    }
+}
