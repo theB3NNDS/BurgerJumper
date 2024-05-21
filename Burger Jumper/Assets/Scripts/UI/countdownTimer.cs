8a2058ccabab72ceba92f422ad1253510bc3ac85
theB3NNDS <78061518+theB3NNDS@users.noreply.github.com>
2024-05-21 21:36:15 +0800

    feat: implement basic countdown mechanic

---
+++ Burger Jumper/Assets/Scripts/UI/countdownTimer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class countdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 60f;
+    [SerializeField] TextMeshProUGUI countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
